abbba58e62e2a282d7c51a676ce3000721b5a793
liaohuancheng <949104781@qq.com>
2019-05-26 19:11:50 +0800

    add Assets

---
+++ Assets/RPG_BOX_Free/Scripts/Managers/BarManager.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class BarManager : MonoBehaviour {
+
+    Image BarImage;
+    Image BarBackground;
+    Outline BarOutline;
+    Text BarText;
+
+    public Color32 BarColor=Color.red;
+    public Color32 BackgroundColor=Color.gray;
+    public Color32 OutlineColor=Color.black;
+    public float OutlineSize;
+    public int TextFontSize;
+    public Color32 TextColor=Color.black;
+    public Vector2 BarSize = new Vector2(100,100);
+    public int PercentageRoundingToNearest;
+
+    public float TestMax;
+    public float TestMin;
+    public bool TestIsPrecentage;
+    public float TestValue;
+    public bool IsShowText;
+
+    private void Start()
+    {
+        GetChildren();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    void GetChildren()
+    {
+        BarImage = transform.Find("TheBar").GetComponent<Image>();
+        BarOutline = transform.Find("BarBG").GetComponent<Outline>();
+        BarBackground = transform.Find("BarBG").GetComponent<Image>();
+        BarText = transform.Find("BarText").GetComponent<Text>();
+    }
+
+    /// <summary>
+    /// 改变bar的颜色
+    /// </summary>
+    public void ChangeLookOfTheBar()
+    {
+        GetChildren();
+
+        BarImage.color = BarColor;
+        BarImage.GetComponent<RectTransform>().sizeDelta = BarSize;
+
+        BarBackground.color = BackgroundColor;
+        BarBackground.GetComponent<RectTransform>().sizeDelta = BarSize;
+        BarOutline.effectColor = OutlineColor;
+        BarOutline.effectDistance = new Vector2(OutlineSize, OutlineSize);
+
+        BarText.fontSize = TextFontSize;
+        BarText.color = TextColor;
+    }
+
+    /// <summary>
+    /// 改变bar尺寸
+    /// </summary>
+    /// <param name="CurrentValue"></param>
+    /// <param name="MaxValue"></param>
+    /// <param name="IsPrecentage"></param>
+    public void ChangeBarSize(float CurrentValue, float MaxValue, bool IsPrecentage)
+    {
+        if (CurrentValue > MaxValue)
+        {
+            CurrentValue = MaxValue;
+        }
+        else if (CurrentValue < 0)
+        {
+            CurrentValue = 0;
+        }
+        BarImage.fillAmount = CurrentValue / MaxValue;
+
+        if (IsShowText)
+        {
+            if (IsPrecentage)
+            {
+                float value = (CurrentValue / MaxValue) * 100;
+                double percentage = Math.Round(value, PercentageRoundingToNearest);
+                BarText.text = (percentage).ToString() + "%";
+            }
+            else
+            {
+                BarText.text = CurrentValue.ToString("0") + " / " + MaxValue.ToString("0");
+            }
+        }
+        else
+        {
+            BarText.text = "";
+        }
+
+    }
+
+}
